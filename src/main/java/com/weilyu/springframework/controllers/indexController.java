9ff02a8265b90faef30a369701af5bc543174d48
Weilyu Wang <weilyuwang@gmail.com>
2019-06-30 11:21:04 -0400

    adding domain files

--- src/main/java/com/weilyu/springframework/controllers/indexController.java
+++ src/main/java/com/weilyu/springframework/controllers/indexController.java
@@ -1,4 +1,14 @@
 package com.weilyu.springframework.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class indexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index"; // this must match the index.html file name under templates folder
+    }
 }
