eb56a713b2cf5bd609e5bc0745784cd183521371
Weilyu Wang <weilyuwang@gmail.com>
2019-07-07 11:32:20 -0400

    Adding RecipeController to view recipe by Id and recipe/show/{id} view

--- src/test/java/com/weilyu/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/com/weilyu/springframework/services/RecipeServiceImplTest.java
@@ -8,9 +8,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
@@ -29,8 +31,24 @@ public class RecipeServiceImplTest {
         recipeService = new RecipeServiceImpl(recipeRepository);
     }
 
+
     @Test
-    public void getRecipes() {
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    public void getRecipesTest() throws Exception{
 
         //set up recipe data
         Recipe recipe = new Recipe();
@@ -38,12 +56,13 @@ public class RecipeServiceImplTest {
         recipesData.add(recipe);
 
         //telling mockito to return our data
-        when(recipeRepository.findAll()).thenReturn(recipesData);
+        when(recipeService.getRecipes()).thenReturn(recipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
         assertEquals(recipes.size(), 1);
         // verify the findAll method of recipeRepository was called only once.
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
     }
 }
