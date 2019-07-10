4cfba8ab1605aaee5d41c7a3f6d32e2100e35715
Weilyu Wang <weilyuwang@gmail.com>
2019-07-10 17:06:26 -0400

    create new recipe

--- src/main/java/com/weilyu/springframework/controllers/RecipeController.java
+++ src/main/java/com/weilyu/springframework/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.weilyu.springframework.controllers;
 
+import com.weilyu.springframework.commands.RecipeCommand;
 import com.weilyu.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -22,4 +25,23 @@ public class RecipeController {
 
         return "recipe/show";
     }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    //The method annotated with @PostMapping("recipe") will get called when the recipeform is submitted.
+
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+
+        //@ModelAttribute tells Spring to bind the form post parameters to the RecipeCommand object
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
 }
