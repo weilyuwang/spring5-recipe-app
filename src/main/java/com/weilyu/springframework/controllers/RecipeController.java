f67881b4253ba341cfb58eac40d6df73234a0757
Weilyu Wang <weilyuwang@gmail.com>
2019-07-13 23:33:58 -0400

    Adding Exception handler

--- src/main/java/com/weilyu/springframework/controllers/RecipeController.java
+++ src/main/java/com/weilyu/springframework/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.weilyu.springframework.controllers;
 
 import com.weilyu.springframework.commands.RecipeCommand;
+import com.weilyu.springframework.exceptions.NotFoundException;
 import com.weilyu.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -56,6 +59,19 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 
 }
