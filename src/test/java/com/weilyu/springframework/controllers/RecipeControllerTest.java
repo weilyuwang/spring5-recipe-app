320dc8a6fa22cefef6c3a89e6e7ecff60db52c88
Weilyu Wang <weilyuwang@gmail.com>
2019-07-13 23:55:12 -0400

    Adding 404 & 400 error html page

--- src/test/java/com/weilyu/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/com/weilyu/springframework/controllers/RecipeControllerTest.java
@@ -60,6 +60,15 @@ public class RecipeControllerTest {
                 .andExpect(view().name("404error"));
     }
 
+
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/abc/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
     @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
